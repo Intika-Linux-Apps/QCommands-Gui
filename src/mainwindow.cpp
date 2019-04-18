d00dc5e5d3b7bbc5e12e54da15405cc235cf3483
intika <intika@librefox.org>
2019-04-18 06:51:46 +0200

    reverse title bar removal

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -84,7 +84,7 @@ MainWindow::MainWindow(TerminalConfig &cfg,
     m_bookmarksDock->setFeatures(QDockWidget::DockWidgetFloatable | QDockWidget::DockWidgetMovable);
           
     // Remove title bar
-    m_bookmarksDock->setTitleBarWidget(new QWidget());
+    //m_bookmarksDock->setTitleBarWidget(new QWidget());
           
     addDockWidget(Qt::LeftDockWidgetArea, m_bookmarksDock);
     connect(bookmarksWidget, SIGNAL(callCommand(QString)),
